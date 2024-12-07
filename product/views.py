0a0ae9dd34065b61fbf1b347f0ab0ec75b5e6fb4
Ivan <iivan.zybin@gmail.com>
2024-12-07 09:05:46 +0300

    product search

--- product/views.py
+++ product/views.py
@@ -1,13 +1,25 @@
+import django_filters
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from product.models import ProductModel
 from product.serializers import ProductSerializer
 
 
+class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = ProductModel
+        fields = ['name']
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = ProductModel.objects.all()
+    filter_backends = (SearchFilter,)
+    search_fields = ['name']
 
     def get_serializer(self, *args, **kwargs):
         """
@@ -16,4 +28,3 @@ class ProductViewSet(viewsets.ModelViewSet):
         """
         serializer_class = self.get_serializer_class()
         return serializer_class(*args, **kwargs)
-
